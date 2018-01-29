c5847917754773e952339b9bd62775fae6110291
Alex <WillKane@dclarer.com>
2018-01-29 05:00:58 +0000

    initial commit of matrix code. This is seriously rough

---
+++ frame_buffer.py
@@ -0,0 +1,151 @@
+import time
+
+#Import neopixel, this is for the LED control
+from neopixel import *
+
+#Import font structure
+from font import px56
+
+# LED strip configuration:
+LED_PIN        = 19      # GPIO pin connected to the pixels (18 uses PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 128     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 1       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
+
+class frameBuffer():
+
+	def __init__(self, x_dim, y_dim):
+		self.x_total = x_dim
+		self.y_total = y_dim
+		self.fb = self.createBuffer(x_dim, y_dim)
+		self.sb = [self.createBuffer(x_dim, 7), self.createBuffer(x_dim, 7)]
+
+		# String definitions
+		self.font = px56()
+		self.draw_string = [True, True]
+		self.max_rows = y_dim//self.font.getCharHeight()
+		self.y2 = self.font.getCharHeight()+1 
+		self.string = ["",""]
+		self.scroll = [False,False]
+		self.pix_offset = [0,0]
+		
+        	# Create NeoPixel object with appropriate configuration.
+		self.strip = Adafruit_NeoPixel(x_dim*y_dim, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+        	# Intialize the library (must be called once before other functions).
+		self.strip.begin()
+
+
+	def createBuffer(self, x, y):
+		buffer = []
+		#Fill Y lists to X size
+		for i in range(y):
+			buffer.append([])
+			for j in range(x):
+				buffer[i].append(0)
+		return buffer
+		
+	def clearBuffer(self):
+		for i in range(self.y_total):
+			for j in range(self.x_total):
+				self.fb[i][j] = 0
+
+	def printXY(self):
+		print('x = ', self.x_total, ',y = ', self.y_total, '\n')
+
+	def insertData(self, buffer, x_orig, y_orig, data):
+		x_size = len(data[0])
+		y_size = len(data)
+		#print(y_orig, x_orig)
+		#print(y_size, x_size)
+		for y in range(y_size):
+			for x in range(x_size):
+				buffer[y_orig + y][x_orig + x] = data[y][x]
+		return buffer
+	
+	def insertStrBufWithRoll(self, buffer, x_orig, y_orig, data, xshift):
+		buf_xsize = len(buffer[0])
+		img_xsize = len(data[0])
+		y_size = len(data)
+		#print(y_orig, x_orig)
+		#print(y_size, x_size)
+		for y in range(y_size):
+			for x in range(buf_xsize):
+				if(x<img_xsize):
+					buffer[y_orig + y][x_orig + x] = data[y][x+xshift]
+				else:
+					buffer[y_orig + y][x_orig + x] = 0
+		return buffer
+
+	def updateStringBuffer(self, buffer, row):
+		x = 0
+		y = row*self.y2 
+		for i in range(len(self.string[row])):
+			char_wid, char_data = self.font.get_char(self.string[row][i])
+			self.insertData(buffer, x, y, char_data)
+			self.insertData(buffer, x+5, y, self.font.get_gap())
+			x += 6
+		#print(self.fb)
+		return buffer
+
+	def clearString(self, row):
+			self.string[row] = ''
+			self.draw_string = False
+
+
+	def writeString(self, string, row):
+		self.string[row] = string
+		self.draw_string = True
+		row_width = 0
+		for i in range(len(string)):
+			if(string[i] == " "):
+				row_width += 1 #Just add a space
+			else:
+				char_wid, char_data = self.font.get_char(string[i])
+				if(row_width + char_wid + 1 > self.x_total):
+					self.scroll[row] = True
+				row_width += char_wid + 1 #Add space plus character width
+		self.sb[row] = self.createBuffer(row_width, self.font.getCharHeight())
+		currx=0
+		for i in range(len(string)):
+			char_wid, char_data = self.font.get_char(string[i])
+			self.sb[row] = self.insertData(self.sb[row], currx, 0, char_data)
+			currx += char_wid + 1
+		
+		return
+
+	def drawPixel(self, x, y):
+		if(self.fb[y][x]):
+			if(y%2 == 0):
+				#self.strip.setPixelColor((y*26)+x, Color(32,32,32))
+				self.strip.setPixelColor((y*26)+x, Color(0,64,0))
+			else:
+				#self.strip.setPixelColor((y*26)+(25-x), Color(32,32,32))
+				self.strip.setPixelColor((y*26)+(25-x), Color(0,64,0))
+		else:
+			if(y%2 == 0):
+				#self.strip.setPixelColor(y*26+x, Color(0,0,0))
+				self.strip.setPixelColor(y*26+x, Color(32,0,0))
+			else:
+				#self.strip.setPixelColor(y*26+(25-x), Color(0,0,0))
+				self.strip.setPixelColor(y*26+(25-x), Color(32,0,0))
+
+	def drawDisplay(self, enable):
+		if(enable == True):
+			if(self.draw_string):
+				#for row in range(2):
+				#	self.sb[row] = self.updateStringBuffer(self.sb[row], row)
+				self.fb = self.insertStrBufWithRoll(self.fb, 0, 0, self.sb[0], self.pix_offset[0])
+				self.fb = self.insertStrBufWithRoll(self.fb, 0, self.y2, self.sb[1], self.pix_offset[1])
+			
+			for i in range(self.y_total):
+				for j in range(self.x_total):
+					self.drawPixel(j, i)
+		else:
+			for i in range(self.strip.numPixels()):
+				self.strip.setPixelColor(i, Color(0,0,0))
+
+		self.strip.show()
+
---
+++ matrix_server.py
@@ -0,0 +1,120 @@
+import time
+from multiprocessing import Process
+
+#Import flash, this is python webserver
+from flask import Flask, render_template
+
+import sqlite3
+
+#Import neopixel, this is for the LED control
+from neopixel import *
+
+#Import custom frame buffer
+from frame_buffer import frameBuffer
+
+#Import datareader for fetching stock info
+import quandl 
+import sys
+from datetime import datetime, date, timedelta
+
+# LED strip configuration:
+LED_COUNT      = 500      # Number of LED pixels.
+LED_PIN        = 19      # GPIO pin connected to the pixels (18 uses PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 128     # Set to 0 for darkest and 255 for brightest LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 1       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
+
+
+#---------------------------------------------------------------------
+# LED Control functions
+#---------------------------------------------------------------------
+#create frame_buffer object
+fb = frameBuffer(26, 15)
+
+
+#---------------------------------------------------------------------
+# Stock fetch functions
+#---------------------------------------------------------------------
+#Need to make stocks dyanmic. Will add to SQL later
+tickers = ['TSLA', 'AMZN', 'FTV', 'BDC']
+today = datetime.today()
+if(today.isoweekday() == 6):
+	close = today-timedelta(days=1)
+elif(today.isoweekday() == 7):
+	close = today-timedelta(days=2)
+else:
+	close = today
+close_string = close.strftime('%Y-%m-%d')
+quandl.ApiConfig.api_key = "QaBJWU-nma8b6HTSzr-s"
+quotes = quandl.get_table('WIKI/PRICES', ticker = tickers,
+                        qopts = { 'columns': ['ticker', 'adj_close'] }, 
+                        date = { 'gte': close_string }, 
+                        paginate=True)
+quotes.set_index('ticker')
+stocks = dict()
+#for i in range(len(tickers)):
+	#stocks[quotes.iloc[i][0]] = quotes.iloc[i][1]
+print(stocks)
+
+#---------------------------------------------------------------------
+# Webserver functions
+#---------------------------------------------------------------------
+#Create flask webserver object
+app = Flask(__name__)
+
+#Create database for status
+db = sqlite3.connect('database.db')
+cursor=db.cursor()
+cursor.execute('''UPDATE dispstat SET status = ? where display = ?''', (0,0))
+db.close()
+
+def server():
+	@app.route('/')
+	def index():
+		return 'Hello world'
+
+	@app.route('/strand/<name>')
+	def hello(name):
+		if(name == 'on'):
+			setStatus(0,1)
+			print("turn display on")
+		elif(name == 'off'):
+			setStatus(0,0)
+		return render_template('page.html', name=name)
+
+	app.run(debug=False, host='0.0.0.0')
+
+	db.close()
+
+
+#create thread
+web_process = Process(target=server)
+
+def setStatus(disp, status):
+	db = sqlite3.connect('database.db')
+	cursor=db.cursor()
+	cursor.execute('''UPDATE dispstat SET status = ? where display = ?''', (status,disp))
+	db.commit()
+	db.close()
+
+
+def getStatus(disp):
+	db = sqlite3.connect('database.db')
+	cursor=db.cursor()
+	cursor.execute('''SELECT status FROM dispstat WHERE display=?''', (disp,))
+	disp_stat = cursor.fetchone()
+	db.close()
+	return disp_stat[0]
+
+if __name__ == '__main__':
+	web_process.start()
+	fb.writeString('Xmas Test', 0)
+	fb.writeString('Test', 1)
+	while(True):
+		enable = getStatus(0)
+		fb.drawDisplay(enable)
+		time.sleep(0.030)
+
+
