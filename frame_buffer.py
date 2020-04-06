3952bf21f5aaa0c0bc8a769826cbe8732ca59d6b
Alex <WillKane@dclarer.com>
2020-04-05 19:32:00 -0700

    Add working scroll and stock ticker

--- frame_buffer.py
+++ frame_buffer.py
@@ -17,135 +17,181 @@ LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
 
 class frameBuffer():
 
-	def __init__(self, x_dim, y_dim):
-		self.x_total = x_dim
-		self.y_total = y_dim
-		self.fb = self.createBuffer(x_dim, y_dim)
-		self.sb = [self.createBuffer(x_dim, 7), self.createBuffer(x_dim, 7)]
+    def __init__(self, x_dim, y_dim):
+        self.x_total = x_dim
+        self.y_total = y_dim
+        #this is the picture data buffer
+        self.fb = self.createBuffer(x_dim, y_dim)
+        #this is the string data buffer
+        self.sb = [self.createBuffer(x_dim, 7), self.createBuffer(x_dim, 7)]
 
-		# String definitions
-		self.font = px56()
-		self.draw_string = [True, True]
-		self.max_rows = y_dim//self.font.getCharHeight()
-		self.y2 = self.font.getCharHeight()+1 
-		self.string = ["",""]
-		self.scroll = [False,False]
-		self.pix_offset = [0,0]
-		
-        	# Create NeoPixel object with appropriate configuration.
-		self.strip = Adafruit_NeoPixel(x_dim*y_dim, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
-        	# Intialize the library (must be called once before other functions).
-		self.strip.begin()
+        # String definitions
+        self.font = px56()
+        self.draw_string = [True, True]
+        self.max_rows = y_dim//self.font.getCharHeight()
+        self.y2 = self.font.getCharHeight()+1 
+        self.string = ["",""]
+        self.scroll = [False,False]
+        self.pix_offset = [0,0]
+        self.string_len = [0,0]
 
-
-	def createBuffer(self, x, y):
-		buffer = []
-		#Fill Y lists to X size
-		for i in range(y):
-			buffer.append([])
-			for j in range(x):
-				buffer[i].append(0)
-		return buffer
-		
-	def clearBuffer(self):
-		for i in range(self.y_total):
-			for j in range(self.x_total):
-				self.fb[i][j] = 0
-
-	def printXY(self):
-		print('x = ', self.x_total, ',y = ', self.y_total, '\n')
-
-	def insertData(self, buffer, x_orig, y_orig, data):
-		x_size = len(data[0])
-		y_size = len(data)
-		#print(y_orig, x_orig)
-		#print(y_size, x_size)
-		for y in range(y_size):
-			for x in range(x_size):
-				buffer[y_orig + y][x_orig + x] = data[y][x]
-		return buffer
-	
-	def insertStrBufWithRoll(self, buffer, x_orig, y_orig, data, xshift):
-		buf_xsize = len(buffer[0])
-		img_xsize = len(data[0])
-		y_size = len(data)
-		#print(y_orig, x_orig)
-		#print(y_size, x_size)
-		for y in range(y_size):
-			for x in range(buf_xsize):
-				if(x<img_xsize):
-					buffer[y_orig + y][x_orig + x] = data[y][x+xshift]
-				else:
-					buffer[y_orig + y][x_orig + x] = 0
-		return buffer
-
-	def updateStringBuffer(self, buffer, row):
-		x = 0
-		y = row*self.y2 
-		for i in range(len(self.string[row])):
-			char_wid, char_data = self.font.get_char(self.string[row][i])
-			self.insertData(buffer, x, y, char_data)
-			self.insertData(buffer, x+5, y, self.font.get_gap())
-			x += 6
-		#print(self.fb)
-		return buffer
-
-	def clearString(self, row):
-			self.string[row] = ''
-			self.draw_string = False
+        #set color option
+        self.bkgnd_color = Color(0,64,0)
+        self.txt_color   = Color(32,0,0)
+        
+            # Create NeoPixel object with appropriate configuration.
+        self.strip = Adafruit_NeoPixel(x_dim*y_dim, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+            # Intialize the library (must be called once before other functions).
+        self.strip.begin()
 
 
-	def writeString(self, string, row):
-		self.string[row] = string
-		self.draw_string = True
-		row_width = 0
-		for i in range(len(string)):
-			if(string[i] == " "):
-				row_width += 1 #Just add a space
-			else:
-				char_wid, char_data = self.font.get_char(string[i])
-				if(row_width + char_wid + 1 > self.x_total):
-					self.scroll[row] = True
-				row_width += char_wid + 1 #Add space plus character width
-		self.sb[row] = self.createBuffer(row_width, self.font.getCharHeight())
-		currx=0
-		for i in range(len(string)):
-			char_wid, char_data = self.font.get_char(string[i])
-			self.sb[row] = self.insertData(self.sb[row], currx, 0, char_data)
-			currx += char_wid + 1
-		
-		return
+    def createBuffer(self, x, y):
+        buffer = []
+        #Fill Y lists to X size
+        for i in range(y):
+            buffer.append([])
+            for j in range(x):
+                buffer[i].append(0)
+        return buffer
+        
+    def clearBuffer(self):
+        for i in range(self.y_total):
+            for j in range(self.x_total):
+                self.fb[i][j] = 0
 
-	def drawPixel(self, x, y):
-		if(self.fb[y][x]):
-			if(y%2 == 0):
-				#self.strip.setPixelColor((y*26)+x, Color(32,32,32))
-				self.strip.setPixelColor((y*26)+x, Color(0,64,0))
-			else:
-				#self.strip.setPixelColor((y*26)+(25-x), Color(32,32,32))
-				self.strip.setPixelColor((y*26)+(25-x), Color(0,64,0))
-		else:
-			if(y%2 == 0):
-				#self.strip.setPixelColor(y*26+x, Color(0,0,0))
-				self.strip.setPixelColor(y*26+x, Color(32,0,0))
-			else:
-				#self.strip.setPixelColor(y*26+(25-x), Color(0,0,0))
-				self.strip.setPixelColor(y*26+(25-x), Color(32,0,0))
+    def printXY(self):
+        print('x = ', self.x_total, ',y = ', self.y_total, '\n')
 
-	def drawDisplay(self, enable):
-		if(enable == True):
-			if(self.draw_string):
-				#for row in range(2):
-				#	self.sb[row] = self.updateStringBuffer(self.sb[row], row)
-				self.fb = self.insertStrBufWithRoll(self.fb, 0, 0, self.sb[0], self.pix_offset[0])
-				self.fb = self.insertStrBufWithRoll(self.fb, 0, self.y2, self.sb[1], self.pix_offset[1])
-			
-			for i in range(self.y_total):
-				for j in range(self.x_total):
-					self.drawPixel(j, i)
-		else:
-			for i in range(self.strip.numPixels()):
-				self.strip.setPixelColor(i, Color(0,0,0))
+    def setBackgroundColor(self, r, g, b):
+        self.bkgnd_color = Color(r, g, b)
 
-		self.strip.show()
+    def setTextColor(self, r, g, b):
+        self.txt_color = Color(r, g, b)
+
+    def insertData(self, buffer, x_orig, y_orig, data, data_width):
+        x_size = data_width
+        y_size = len(data)
+        #print(y_orig, x_orig)
+        #print(y_size, x_size)
+        for y in range(y_size):
+            for x in range(x_size):
+                buffer[y_orig + y][x_orig + x] = data[y][x]
+        return buffer
+    
+    def insertStrBufWithRoll(self, buffer, x_orig, y_orig, data, xshift):
+        buf_xsize = len(buffer[0])
+        img_xsize = len(data[0])
+        y_size = len(data)
+        #print(y_orig, x_orig)
+        #print(y_size, x_size)
+        for y in range(y_size):
+            for x in range(buf_xsize):
+                if(x<img_xsize):
+                    #Deal with pixel wrapping
+                    if(x+xshift >= img_xsize):
+                        x_pos = (x+xshift) - img_xsize
+                    else: 
+                        x_pos = x+xshift
+                    buffer[y_orig + y][x_orig + x] = data[y][x_pos]
+                else:
+                    buffer[y_orig + y][x_orig + x] = 0
+        return buffer
+
+    def updateStringBuffer(self, buffer, row):
+        x = 0
+        y = row*self.y2 
+        for i in range(len(self.string[row])):
+            char_wid, char_data = self.font.get_char(self.string[row][i])
+            self.insertData(buffer, x, y, char_data, char_wid)
+            self.insertData(buffer, x+5, y, self.font.get_gap(), char_wid)
+            x += 6
+        #print(self.fb)
+        return buffer
+
+    def clearString(self, row):
+            self.string[row] = ''
+            self.draw_string = False
+
+
+    def writeString(self, string, row):
+        self.string[row] = string
+        self.draw_string = True
+        self.scroll[row] = False #start with assuming string fits
+        row_width = 0
+        for i in range(len(string)):
+            if(string[i] == " "):
+                row_width += 1 #Just add a space
+            else:
+                char_wid, char_data = self.font.get_char(string[i])
+                if(row_width + char_wid + 1 > self.x_total):
+                    self.scroll[row] = True
+                    row_width += 5 # add a bunch of space at the end
+                row_width += char_wid + 1 #Add space plus character width
+                #print(string[i], char_wid, row_width)
+        self.sb[row] = self.createBuffer(row_width, self.font.getCharHeight())
+        self.string_len[row] = row_width
+        currx=0
+        for i in range(len(string)):
+            char_wid, char_data = self.font.get_char(string[i])
+            self.sb[row] = self.insertData(self.sb[row], currx, 0, char_data, char_wid)
+            currx += char_wid + 1
+
+        #if we're scrolling, add a space to the end to keep text seperate
+        if(self.scroll[row] == True):
+            char_wid, char_data = self.font.get_char(" ")
+            self.sb[row] = self.insertData(self.sb[row], currx, 0, char_data, 1)
+        
+        return
+
+    def drawPixel(self, x, y):
+        if(self.fb[y][x]):
+            if(y%2 == 0):
+                #self.strip.setPixelColor((y*26)+x, Color(32,32,32))
+                self.strip.setPixelColor((y*26)+x, self.txt_color)
+            else:
+                #self.strip.setPixelColor((y*26)+(25-x), Color(32,32,32))
+                self.strip.setPixelColor((y*26)+(25-x), self.txt_color)
+        else:
+            if(y%2 == 0):
+                #self.strip.setPixelColor(y*26+x, Color(0,0,0))
+                self.strip.setPixelColor(y*26+x, self.bkgnd_color)
+            else:
+                #self.strip.setPixelColor(y*26+(25-x), Color(0,0,0))
+                self.strip.setPixelColor(y*26+(25-x), self.bkgnd_color)
+
+    def updateOffset(self, amount):
+        #print(self.pix_offset[row], amount, self.string_len[row])
+        wrap_complete = [False, False]
+        for row in range(len(self.sb)):
+            #only roll if the buffer is bigger than the display
+            if(self.scroll[row]  == True):
+                if((self.pix_offset[row] + amount) > self.string_len[row]):
+                    #if((self.pix_offset[row] + amount + self.x_total) > self.string_len[row]):
+                    # Wrap once we reach the end of the buffer. This will just scroll text in a circle
+                    self.pix_offset[row] = 0
+                    wrap_complete[row] = True
+                else:
+                    self.pix_offset[row] =  self.pix_offset[row] + amount;
+            else:
+                self.pix_offset[row] = 0
+                wrap_complete[row] = True
+                
+        return wrap_complete 
+
+    def drawDisplay(self, enable):
+        if(enable == True):
+            if(self.draw_string):
+                #for row in range(2):
+                #    self.sb[row] = self.updateStringBuffer(self.sb[row], row)
+                self.fb = self.insertStrBufWithRoll(self.fb, 0, 0, self.sb[0], self.pix_offset[0])
+                self.fb = self.insertStrBufWithRoll(self.fb, 0, self.y2, self.sb[1], self.pix_offset[1])
+            
+            for i in range(self.y_total):
+                for j in range(self.x_total):
+                    self.drawPixel(j, i)
+        else:
+            for i in range(self.strip.numPixels()):
+                self.strip.setPixelColor(i, Color(0,0,0))
+
+        self.strip.show()
 
--- matrix_server.py
+++ matrix_server.py
@@ -53,20 +53,27 @@ def stockPrice(symbol):
     data = json.loads(stockGet(symbol))
     return str(data["latestPrice"])
 
+def isStockUp(symbol):
+    data = json.loads(stockGet(symbol))
+    if(data['change'] > 0):
+        return True
+    else:
+        return False
+
 #Need to make stocks dyanmic. Will add to SQL later
 tickers = ['TSLA', 'AMZN', 'FTV', 'BDC', 'AAPL']
 today = datetime.today()
 if(today.isoweekday() == 6):
-	close = today-timedelta(days=1)
+    close = today-timedelta(days=1)
 elif(today.isoweekday() == 7):
-	close = today-timedelta(days=2)
+    close = today-timedelta(days=2)
 else:
-	close = today
+    close = today
 close_string = close.strftime('%Y-%m-%d')
 
 for stock in tickers:
     current_price = stockPrice(stock)
-    print(stock + " = " + current_price)
+    print(stock + " = " + current_price, isStockUp(stock))
 
 #---------------------------------------------------------------------
 # Webserver functions
@@ -83,52 +90,82 @@ db.close()
 
 
 def server():
-	@app.route('/')
-	def index():
-		return 'Hello world'
+    @app.route('/')
+    def index():
+        return 'Hello world'
 
-	@app.route('/strand/<name>')
-	def hello(name):
-		if(name == 'on'):
-			setStatus(0,1)
-			print("turn display on")
-		elif(name == 'off'):
-			setStatus(0,0)
-		return render_template('page.html', name=name)
+    @app.route('/strand/<name>')
+    def hello(name):
+        if(name == 'on'):
+            setStatus(0,1)
+            print("turn display on")
+        elif(name == 'off'):
+            setStatus(0,0)
+        return render_template('page.html', name=name)
 
-	app.run(debug=False, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0')
 
-	db.close()
+    db.close()
 
 
 #create thread
 web_process = Process(target=server)
 
 def setStatus(disp, status):
-	db = sqlite3.connect('database.db')
-	cursor=db.cursor()
-	cursor.execute('''UPDATE dispstat SET status = ? where display = ?''', (status,disp))
-	db.commit()
-	db.close()
+    db = sqlite3.connect('database.db')
+    cursor=db.cursor()
+    cursor.execute('''UPDATE dispstat SET status = ? where display = ?''', (status,disp))
+    db.commit()
+    db.close()
 
 
 def getStatus(disp):
-	db = sqlite3.connect('database.db')
-	cursor=db.cursor()
-	cursor.execute('''SELECT status FROM dispstat WHERE display=?''', (disp,))
-	disp_stat = cursor.fetchone()
-	db.close()
-	return disp_stat[0]
+    db = sqlite3.connect('database.db')
+    cursor=db.cursor()
+    cursor.execute('''SELECT status FROM dispstat WHERE display=?''', (disp,))
+    disp_stat = cursor.fetchone()
+    db.close()
+    return disp_stat[0]
+
+def displayStock(stock):
+    if(isStockUp(stock)):
+        fb.setTextColor(64, 0, 0)
+    else:
+        fb.setTextColor(0, 64, 0)
+    fb.writeString(stock, 0)
+    fb.writeString(stockPrice(stock), 1)
+
 
 if __name__ == '__main__':
-	web_process.start()
-	#fb.writeString('Xmas Test', 0)
-	#fb.writeString('Test', 1)
-	fb.writeString('AAPL', 0)
-	fb.writeString(stockPrice('AAPL'), 1)
-	while(True):
-		enable = getStatus(0)
-		fb.drawDisplay(enable)
-		time.sleep(0.030)
+    web_process.start()
+    #fb.writeString('Xmas Test', 0)
+    #fb.writeString('Test', 1)
+    fb.setBackgroundColor(0, 0, 0)
+
+    #setup stock ticker stuff
+    seconds = 0
+    stock_index = 2
+    stock_string = tickers[stock_index]
+    displayStock(stock_string)
+
+    while(True):
+        enable = getStatus(0)
+        fb.drawDisplay(enable)
+        wrap_complete = fb.updateOffset(1)
+        #time.sleep(0.030)
+        time.sleep(0.5)
+
+        # Wait for at least 5 seconds and for the price to stop wrapping
+        seconds += 1;
+        if(wrap_complete[1] == True and seconds > 5):
+            if(stock_index < len(tickers)-1):
+                stock_index = stock_index + 1;
+            else:
+                stock_index = 0
+            stock_string = tickers[stock_index]
+            #print(stock_string, stockPrice(stock_string))
+            displayStock(stock_string)
+            seconds = 0
+
 
 
